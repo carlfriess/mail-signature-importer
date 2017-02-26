49d2205fb7722935d7c8b31b315fcb971119d797
Carl Friess <carl.friess@me.com>
2017-02-26 18:01:52 +0100

    Initial Commit

---
+++ Mail Signature Importer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mail Signature Importer
+//
+//  Created by Carl Friess on 26/02/2017.
+//  Copyright © 2017 Carl Friess. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mail Signature Importer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mail Signature Importer
+//
+//  Created by Carl Friess on 26/02/2017.
+//  Copyright © 2017 Carl Friess. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
